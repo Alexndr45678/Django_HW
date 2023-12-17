34b1eb4aa67fa65704f227d3ed9f31d9fbc7b244
Alexandr <111227400+Alexndr45678@users.noreply.github.com>
2023-12-17 22:11:53 +0300

    HW_1 done

---
+++ HW_1/myproject/myapp/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+import logging
+
+# Create your views here.
+logger = logging.getLogger(__name__)
+
+
+def log(view_func):
+    def wrapper(request):
+        logger.info(f"The view function was called {view_func.__name__}")
+        return view_func(request)
+
+    return wrapper
+
+
+@log
+def main(request):
+    try:
+        result = 1 / 0
+    except Exception as e:
+        logger.exception(f"Error! {e}")
+        return HttpResponse("My first site")
+
+
+@log
+def about(request):
+    logger.info("Main page accessed.")
+    return HttpResponse("This page contain info about me.")
